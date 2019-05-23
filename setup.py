9cf3bca60a7efa98e7b51e30ae498afc85342d1e
Andrew Nadolski <andrew.nadolski@gmail.com>
2019-05-23 17:34:07 -0500

    Add a basic setup.py
    This is highly experimental. Not sure it is going to do the things I
    need it to do just yet, but the bones are there.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='apytl',
+      version='0.1a0',
+      author='Andrew Nadolski',
+      author_email='andrew.nadolski@gmail.com',
+      platforms=['Linux', 'Mac OS X'],
+      python_requires='>=3.6',
+      package_dir={'apytl': 'apytl'},
+      packages=['apytl'],
+      )
