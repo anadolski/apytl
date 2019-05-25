5d20558f84be9ec920e0b8de6c3088d1107c72dc
Andrew Nadolski <andrew.nadolski@gmail.com>
2019-05-25 16:24:41 -0500

    Make the example bar script work with refactor
    Fixed imports and instantiations so the example script "Just Works" when
    it is run. I've also cut down the pause between iterations so it runs a
    little faster, and reorganized the order in which the bars are drawn.

--- apytl/tests/example_bars.py
+++ apytl/tests/example_bars.py
@@ -1,35 +1,35 @@
 import time
-
 import apytl
 
 NTOTAL = 25
-WAIT = 0.1
+WAIT = 0.01
 
-
-for emojikey, emojicode in apytl._EMOJI.items():
-    tstring = 'Are these {} emojis?'.format(emojikey)
-    vs = 'V'*len(tstring)
-    print(tstring)
-    print(vs)
-    for ind, val in enumerate(range(NTOTAL)):
-        apytl.drawbar(ind, NTOTAL, fill=emojicode)
-        time.sleep(WAIT)
-    print('')
+TEST_BAR = apytl.Bar()
 
 tstring = 'Are these octothorpes?'
 vs = 'V'*len(tstring)
 print(tstring)
 print(vs)
 for ind, val in enumerate(range(NTOTAL)):
-    apytl.drawbar(ind, NTOTAL)
+    TEST_BAR.drawbar(ind, NTOTAL)
     time.sleep(WAIT)
 print('')
+
+for emojikey, emojicode in TEST_BAR._EMOJI.items():
+    tstring = 'Are these {} emojis?'.format(emojikey)
+    vs = 'V'*len(tstring)
+    print(tstring)
+    print(vs)
+    for ind, val in enumerate(range(NTOTAL)):
+        TEST_BAR.drawbar(ind, NTOTAL, fill=emojicode)
+        time.sleep(WAIT)
+    print('')
 
 tstring = 'Are these randomly selected?'
 vs = 'V'*len(tstring)
 print(tstring)
 print(vs)
 for ind, val in enumerate(range(NTOTAL)):
-    apytl.drawbar(ind, NTOTAL, fill='random')
+    TEST_BAR.drawbar(ind, NTOTAL, fill='random')
     time.sleep(WAIT)
 print('')
